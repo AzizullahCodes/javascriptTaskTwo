5e593a410ba7e61b515a7e56d68f050ee36133f5
Azizullah <azizullahcodes@gmail.com>
2026-01-21 21:14:11 +0500

    upd

--- signupLogin/app.js
+++ signupLogin/app.js
@@ -2,20 +2,17 @@
 function login(){
     let email = document.getElementById('emailInput').value;
     let password = document.getElementById('passwordInput').value;
-    // console.log(email);
-    // console.log(password)
+    
 
     // now we get data from localstorage to compare email and password 
     let getInfoFromLocalstorage = localStorage.getItem('signupInformation');
-    // console.log(getInfoFromLocalstorage)
 // now we convert the data of localstorage to Json.parse()
 let jsonData = JSON.parse(getInfoFromLocalstorage);
 console.log(jsonData);
 let jsonEmail = jsonData.email;
 let jsonPassword = jsonData.password;
-// console.log(jsonEmail);
-// console.log(jsonPassword);
-if(email == jsonEmail && password == jsonPassword){
+
+if((email !== '' && password !== '')&&(email == jsonEmail && password == jsonPassword)){
     window.location.href = './home.html'
 }
 else{
